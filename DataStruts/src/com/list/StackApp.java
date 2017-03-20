74a3533e0325c0bf24391cb706142b75a1caff42
lailonghui <605403092@qq.com>
2017-03-20 13:00:30 +0800

    infix to postfix

--- DataStruts/src/com/list/StackApp.java
+++ DataStruts/src/com/list/StackApp.java
@@ -11,6 +11,7 @@ public class StackApp {
 		
 		System.out.println(stackApp.reverse("Hello World"));
 		System.out.println(stackApp.calculateReverPolish("6 5 2 3 + 8 * + 3 + *"));
+		System.out.println(stackApp.infixToPostfix("a + b * c + ( d * e + f ) * g"));
 	}
 	
 	/**
@@ -40,7 +41,7 @@ public class StackApp {
 	 * 
 	 * @param expression
 	 * @param result of the Postfix Expressions. 
-	 * */
+	 */
 	public BigDecimal calculateReverPolish(String expression) {
 		
 		String[] expArr = expression.split(" ");
@@ -74,15 +75,52 @@ public class StackApp {
 	}
 	
 	/**
-	 * if s is operator
+	 * if opera is operator
 	 * 
 	 * @param s
 	 * @return true if s is operator, vice verse.
 	 */
-	public boolean isOperator(String s) {
-		return s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/");
+	private boolean isOperator(String opera) {
+		return opera.equals("+") || opera.equals("-") || opera.equals("*") || opera.equals("/");
 	}
 	
+	/**
+	 * 
+	 * @param infix
+	 * @return
+	 */
+	public String infixToPostfix(String infix) {
+		StringBuilder postfix = new StringBuilder();
+		MyArrayList<String> stack = new MyArrayList<>();
+		
+		String[] sArr = infix.split(" "); 
+		
+		for(String s : sArr) {
+			if(isOperator(s)) {
+//				isPreceding(s) ? stack.push(s):stack.pop();
+				if(lowerPriority(s))
+					stack.push(s); 
+				else  
+					postfix.append(stack.pop());
+			}else {
+				postfix.append(s);
+			}
+			
+			
+		}
+		
+		return postfix.toString();
+		
+	}
 	
+	/**
+	 * whether operator has a lower priority
+	 * 
+	 * @param operator
+	 * @return true if operator has a lower priority the element of top stack 
+	 */
+	private boolean lowerPriority(String operator) {
+		return false;
+	}
 
 }
